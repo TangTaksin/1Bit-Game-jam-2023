d0f0c51a90e04c2593233ff70795c1c64af5bc5b
ResNeedle <110387872+ResNeedle@users.noreply.github.com>
2023-11-15 02:56:54 +0700

    fixed floor switch

--- Scripts/Switch Related/Switch_Floor.gd
+++ Scripts/Switch Related/Switch_Floor.gd
@@ -1,10 +1,25 @@
 extends Switch
+class_name Floor_Switch
 
 @export var isToggle : bool
+var bodyCount : int = 0
 
 func _on_body_entered(body: Node2D) -> void:
-	flipTheSwitch()
+	bodyCount += 1
+	
+	if isToggle :
+		flipTheSwitch()
+	else :
+		bodyCheck()
 
 func _on_body_exited(body: Node2D) -> void:
+	bodyCount -= 1
 	if !isToggle :
-		flipTheSwitch()
+		bodyCheck()
+
+func bodyCheck() :
+	if bodyCount > 0 :
+		set_switch_state(true)
+	else :
+		set_switch_state(false)
+		pass
